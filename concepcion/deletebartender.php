329bb44c945a5ce046447f662f24ed377286b5c7
22-1-02568 <167276614+22-1-02568@users.noreply.github.com>
2024-10-22 19:44:35 +0800

    Delete concepcion directory

--- concepcion/deletebartender.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'core/models.php';
-
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    deleteBartender($conn, $id);
-    header("Location: index.php");
-}
-?>
--- concepcion/deletecustomer.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'core/models.php';
-
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    deleteCustomer($conn, $id);
-    header("Location: index.php");
-}
-?>
